8b549e9c3bbeba21ea830dea522fca22f616f942
playmaker432 <95470948+playmaker432@users.noreply.github.com>
2024-02-07 15:18:46 +0800

    Ver 2.1.0

--- ko_screenshot.py
+++ ko_screenshot.py
@@ -63,19 +63,19 @@ def printSingleLine(input):
     print(input)
     print('--------------------------------------------------')
 
-def separate_address(json_data, chinese_address_series, english_address_series):
-    for i in range(0, json_data.__len__()):
-        content = json_data[i]['text']
-        last_slash = content.rfind('/')
+# def separate_address(json_data, chinese_address_series, english_address_series):
+#     for i in range(0, json_data.__len__()):
+#         content = json_data[i]['text']
+#         last_slash = content.rfind('/')
 
-        chinese_address = content[0:last_slash]
-        english_address = content[last_slash+1:]
+#         chinese_address = content[0:last_slash]
+#         english_address = content[last_slash+1:]
         
-        chinese_address_series = chinese_address_series._append(pd.Series([chinese_address]), ignore_index=True)
-        english_address_series = english_address_series._append(pd.Series([english_address]), ignore_index=True)
-        print(chinese_address, english_address)
+#         chinese_address_series = chinese_address_series._append(pd.Series([chinese_address]), ignore_index=True)
+#         english_address_series = english_address_series._append(pd.Series([english_address]), ignore_index=True)
+#         print(chinese_address, english_address)
 
-    return chinese_address_series, english_address_series
+#     return chinese_address_series, english_address_series
 
 def separate_contact(json_data, telephone_series, contact_series, page_series):
     global photo_cnt
@@ -122,14 +122,6 @@ def upload_file(driver, file_path):
         print('The program is terminated...')
         exit()
 
-def clone_file(srcPath, destPath):
-    fileExistOrCreate(destPath)
-    files = os.listdir(srcPath)
-    for file in files:
-        print(f'Copying file: {file} ...')
-        shutil.copy(os.path.join(srcPath, file), destPath)
-        os.remove(os.path.join(srcPath, file))
-
 def current_time():
     return time.strftime("%Y%m%d-%H%M%S")
 
@@ -138,27 +130,26 @@ def check_inputLens():
 
     root = tkinter.Tk()
 
-    address_files = os.listdir(user.address_path)
+    # address_files = os.listdir(user.address_path)
     contact_files = os.listdir(user.contact_path)
 
-    print('Files in the Address folder:\n', address_files)
+    # print('Files in the Address folder:\n', address_files)
     print('\nFiles in the Contact folder:\n', contact_files)
 
-    if (len(address_files) == 0 and len(contact_files) == 0):
-        tkinter.messagebox.showinfo('Error', f'Please input photos in the address & contact folders.', parent = root)
-        exit()
-    elif(len(address_files) != len(contact_files)):
-        confirm = messagebox.askquestion('Confirmation', "The number of files in the address folder and contact folder are not the same!\nConfirm to proceed?", parent = root)
-        if confirm == 'no':
-            print("Exit the program...")
-            exit()
+    if (len(contact_files) == 0):
+        tkinter.messagebox.showinfo('Error', f'Please input photos in the Contact folder.', parent = root)
+    # elif(len(address_files) != len(contact_files)):
+    #     confirm = messagebox.askquestion('Confirmation', "The number of files in the address folder and contact folder are not the same!\nConfirm to proceed?", parent = root)
+    #     if confirm == 'no':
+    #         print("Exit the program...")
+    #         exit()
 
 def load_samplePhotos():
     global user
-    if(os.path.exists(user.address_path) == False):
-        fileExistOrCreate(user.address_path)
-        shutil.copy('demoAddress_1.png', user.address_path)
-        shutil.copy('demoAddress_2.png', user.address_path)
+    # if(os.path.exists(user.address_path) == False):
+    #     fileExistOrCreate(user.address_path)
+    #     shutil.copy('demoAddress_1.png', user.address_path)
+    #     shutil.copy('demoAddress_2.png', user.address_path)
     
     if(os.path.exists(user.contact_path) == False):
         fileExistOrCreate(user.contact_path)
@@ -175,8 +166,8 @@ def on_key_event(event):
 def generate_output(address_df):
     global user
     fileName = 'Result' + current_time() + '.xlsx'
-    user.input_path = os.path.join(r'C:\Users', user.username, 'Pictures\Greenshots_input')
-    user.output_path = os.path.join(r'C:\Users', user.username, f'Pictures\Greenshots_output')
+    # user.input_path = os.path.join(r'C:\Users', user.username, 'Pictures\Greenshots_input')
+    # user.output_path = os.path.join(r'C:\Users', user.username, f'Pictures\Greenshots_output')
     output_file_path = os.path.join(r'C:\Users', user.username, f'Pictures\Greenshots_output', fileName)
     
     if not os.path.exists(user.output_path):
@@ -186,7 +177,7 @@ def generate_output(address_df):
 
     Popen(output_file_path, shell=True)
 
-    clone_file(user.address_path, os.path.join(user.input_path, f'Greenshots_address_backup_{current_time()}'))
+    # clone_file(user.address_path, os.path.join(user.input_path, f'Greenshots_address_backup_{current_time()}'))
     clone_file(user.contact_path, os.path.join(user.input_path, f'Greenshots_contact_backup_{current_time()}'))
     clone_file(user.fullscreen_path, os.path.join(user.input_path, f'Greenshots_fullscreen_backup_{current_time()}'))
     
@@ -234,19 +225,19 @@ def driver_setup():
 
         check_inputLens()
 
-        try:
-            # address_files_sorted = sorted(address_files,key=lambda x: int(os.path.splitext(x)[0]))
-            address_files_sorted = sorted(address_files, key=lambda x: int(re.search(r'\d+', os.path.splitext(x)[0]).group()))
-            for file in address_files_sorted:
-                print('\nUploading the file: ' + file)
-                file_path = os.path.join(user.address_path, file)
-                json_data = upload_file(driver, file_path)
-                chinese_address_series, english_address_series = separate_address(json_data, chinese_address_series, english_address_series)
+        # try:
+        #     # address_files_sorted = sorted(address_files,key=lambda x: int(os.path.splitext(x)[0]))
+        #     address_files_sorted = sorted(address_files, key=lambda x: int(re.search(r'\d+', os.path.splitext(x)[0]).group()))
+        #     for file in address_files_sorted:
+        #         print('\nUploading the file: ' + file)
+        #         file_path = os.path.join(user.address_path, file)
+        #         json_data = upload_file(driver, file_path)
+        #         chinese_address_series, english_address_series = separate_address(json_data, chinese_address_series, english_address_series)
 
-        except Exception as e:
-            tkinter.messagebox.showinfo('Error', f'Error occurs: {e}\n The program is terminated.', parent = root)
-            # driver.quit()
-            exit()
+        # except Exception as e:
+        #     tkinter.messagebox.showinfo('Error', f'Error occurs: {e}\n The program is terminated.', parent = root)
+        #     # driver.quit()
+        #     exit()
             
         try:
             # contact_files_sorted = sorted(contact_files,key=lambda x: int(os.path.splitext(x)[0]))
@@ -269,6 +260,9 @@ def driver_setup():
     except Exception as e:
         tkinter.messagebox.showinfo('Error', f'Error occurs: {e}\n The program is terminated.', parent = root)
 
+    finally:
+        driver.quit()
+
 def driver_eprc():
     desired_zoom_level = 1.5
     options = webdriver.ChromeOptions()
@@ -277,14 +271,13 @@ def driver_eprc():
     driver.maximize_window()
     driver.execute_script(f"document.body.style.zoom='80%';")
     driver.get('https://eprc.com.hk/eprcLogin.html')
-    
+
 class SimpleUI:
     global user
 
-
     def __init__(self, master):
         self.master = master
-        master.title("Simple UI")
+        master.title("EPRC Helper v2.1")
 
         # Set a larger font size
         font_size = 12
@@ -292,26 +285,34 @@ class SimpleUI:
         self.label = tk.Label(master, text="Choose an option:", pady=10, font=("Arial", font_size))
         self.label.pack()
 
-        self.button4 = tk.Button(master, text="1. Open Input Folder", command=self.open_input_folder, pady=5, font=("Arial", font_size))
-        self.button4.pack(pady=5)
-
-        self.button5 = tk.Button(master, text="2. Open Output Folder", command=self.open_output_folder, pady=5, font=("Arial", font_size))
-        self.button5.pack(pady=5)
-
-        self.button1 = tk.Button(master, text="3. Check/Change Username", command=self.check_change_username, pady=5, font=("Arial", font_size))
+        self.button1 = tk.Button(master, text="1. Open Input Folder", command=self.open_input_folder, pady=5, font=("Arial", font_size))
         self.button1.pack(pady=5)
 
-        self.button2 = tk.Button(master, text="4. Open EPRC Website", command=self.driver_eprc, pady=5, font=("Arial", font_size))
+        self.button2 = tk.Button(master, text="2. Open Output Folder", command=self.open_output_folder, pady=5, font=("Arial", font_size))
         self.button2.pack(pady=5)
 
-        self.button3 = tk.Button(master, text="5. Screenshot EPRC", command=self.eprc_Screenshot, pady=5, font=("Arial", font_size))
+        self.button3 = tk.Button(master, text="3. Check/Change Username", command=self.check_change_username, pady=5, font=("Arial", font_size))
         self.button3.pack(pady=5)
 
-        self.button4 = tk.Button(master, text="6. OCR by Vision Studio", command=driver_setup, pady=5, font=("Arial", font_size))
+        self.button4 = tk.Button(master, text="4. Open EPRC Website\n(Rmb to Log Out before exit)", command=self.driver_eprc, pady=5, font=("Arial", font_size))
         self.button4.pack(pady=5)
+
+        self.button5 = tk.Button(master, text="5. Screenshot EPRC", command=self.eprc_Screenshot, pady=5, font=("Arial", font_size))
+        self.button5.pack(pady=5)
+
+        self.button6 = tk.Button(master, text="6. OCR by Vision Studio", command=driver_setup, pady=5, font=("Arial", font_size))
+        self.button6.pack(pady=5)
+
+        # Bind the F2 key to stop the automation
+        master.bind('<F2>', self.stop_automation)
 
         # When the simpleUI is closed, ask the user first if they want to close the program
         master.protocol("WM_DELETE_WINDOW", self.on_closing)
+    
+    def stop_automation(self, event):
+        global stop_automation
+        print("Pressed 'F2', stop_automation value is set to True.")
+        stop_automation = True
 
     def driver_eprc(self):
         # Disable the button to prevent multiple clicks during the operation
@@ -323,59 +324,77 @@ class SimpleUI:
         global stop_automation
         global page_cnt
         global user
-        global query_name
+        global query_name   
 
-        # Use a suer input dialog to get the query name
-        query_name = simpledialog.askstring("Query Name", "Enter a query name:")
+        stop_automation = False
+        page_cnt = 1
 
-        tkinter.messagebox.showinfo('Information', 'Start Searching in EPRC.')
+        # While loop to keep asking for the query name
+        while True:
+            query_name = simpledialog.askstring("Query Name", "Enter a query name:")
+            if query_name is None:  # Check if dialog is canceled
+                messagebox.showwarning('Warning', 'Operation canceled.')
+                return  # Exit the method if canceled
+            elif not query_name:
+                messagebox.showwarning('Warning', 'Query name cannot be empty.')
+                continue
+            else:
+                break
+
+
+        # Use yes or no dialog to confirm continue
+        confirm = messagebox.askyesno('Confirmation', f'Are you sure to start the screenshot for the query: {query_name}?')
+        if not confirm:
+            return
+
         self.master.withdraw()
 
-        pyautogui.click()
-
         while not stop_automation:
-            time.sleep(3.0)
+            time.sleep(5.0)
+            # Click the 'Next Page' button
+            x_pos = 1785
+            y_pos = 1025
 
             print("Page " + str(page_cnt) + ":")
 
             pyautogui.press('F4')
 
             # Screenshoting the page
-            pyautogui.moveTo(1100, 1010, duration = 0.5)
-            pyautogui.dragTo(830, 255, duration=2.5)
+            pyautogui.moveTo(1350, 1010, duration = 0.3)
+            pyautogui.dragTo(830, 255, duration=1.5)
             
             time.sleep(0.5)
-            pyautogui.typewrite(user.contact_path + f"\\Greenshots_contact_{page_cnt}.png")
+            pyautogui.typewrite(user.contact_path + f"\\{query_name}_{page_cnt}.png")
             time.sleep(0.5)
             pyautogui.press('enter')
 
-            time.sleep(2.0)
+            time.sleep(0.5)
 
             # press the control + print scrn button to capture the full screen
             pyautogui.hotkey('ctrl', 'printscreen')
             time.sleep(0.5)
-            pyautogui.typewrite(user.input_path + f"\\Greenshots_fullscreen\\{page_cnt}.png")
+            pyautogui.typewrite(user.fullscreen_path + f"\\{query_name}_{page_cnt}.png")
             pyautogui.press('enter')
             
-
-            time.sleep(2.0)
-
-            # Cancel the message box in the corner
-            pyautogui.click(1873, 969)
             time.sleep(0.5)
 
-            # Click the 'Next Page' button
-            pyautogui.moveTo(1795, 1025, duration=0.5)
-            cursor = win32gui.GetCursorInfo()
-            print(cursor)
-            
-            if cursor[1] == 65567 or cursor[1] == 65597:
-                pyautogui.click()
-                print('Clicked the \'Next Page\' button!')
-                page_cnt += 1
-            else:
-                print('Cannot find any \'Next Page\' button!')
-                break
+            while True:
+                pyautogui.moveTo(x_pos, y_pos, duration=0.5)
+                cursor = win32gui.GetCursorInfo()
+                cursor_pos = pyautogui.position()
+                print(cursor)
+                
+                if self.within_clickable_area(cursor_pos[0], cursor[1]):
+                    pyautogui.click()
+                    print('Clicked the \'Next Page\' button!')
+                    page_cnt += 1
+                    break
+
+                if(x_pos > 1800):
+                    stop_automation = True
+                    break
+
+                x_pos += 3
 
         print(f'\nFinished. There are {page_cnt} pages in total.')
         self.master.deiconify()
@@ -431,7 +450,19 @@ class SimpleUI:
             subprocess.run(['explorer', user.output_path], check=True)
         except subprocess.CalledProcessError as e:
             print(f"Error opening directory: {e}")
-            
+
+    # Function to check if cursor is within the clickable area
+    def within_clickable_area(self, x, y):
+        return 1785 <= x <= 1800 and (y == 65567 or y == 65597)
+
+
+#A function to check whether a prefix exist in files in a folder
+def check_prefix_exist(prefix, folder):
+    files = os.listdir(folder)
+    for file in files:
+        if file.startswith(prefix):
+            return True
+    return False
 
 def check_username():
     username = readTxt("username.txt").strip()  
@@ -446,27 +477,32 @@ def check_username():
 
 def main():
     global user
+    
+    try: 
+        # Set up for the alert UI
+        root = tkinter.Tk()
+        root.geometry("300x375")
+        root.eval('tk::PlaceWindow . center')
 
-    # Set up for the alert UI
-    root = tkinter.Tk()
-    root.geometry("300x350")
-    root.eval('tk::PlaceWindow . center')
+        username = check_username()
+        user = User(username)
+        user.printUserInformation()
+        fileExistOrCreate(user.input_path)
+        fileExistOrCreate(user.fullscreen_path)
+        load_samplePhotos()
 
-    username = check_username()
-    user = User(username)
-    user.printUserInformation()
-    fileExistOrCreate(user.input_path)
-    fileExistOrCreate(user.fullscreen_path)
-    load_samplePhotos()
+        print('\n...The program is running...')
 
-    print('\n...The program is running...')
+        app = SimpleUI(root)
+        root.mainloop()    
 
-    app = SimpleUI(root)
-    root.mainloop()    
+    except Exception as e:
+        tkinter.messagebox.showinfo('Error', f'Error occurs: {e}\n The program is terminated.', parent = root)
 
-    # End the program
-    print('The program is terminated...')
-    exit()
+    finally:
+        # End the program
+        print('The program is terminated...')
+        exit()
 
 if __name__ == "__main__":
     main()
--- testauto.py
+++ testauto.py
@@ -18,11 +18,12 @@ def main():
     root = tk.Tk()
     root.withdraw()  # Hide the main window
 
-    pyautogui.press('F4')
-# Screenshoting the page
-    pyautogui.moveTo(1186, 1010, duration = 0.5)
-    pyautogui.dragTo(830, 255, duration=2.5)
-    time.sleep(0.5)
+    cursor = win32gui.GetCursorInfo()
+    cursor_pos = pyautogui.position()
+    print(cursor)
+
+    # print cursor
+    print(pyautogui.position())
 
 if __name__ == "__main__":
     main()
