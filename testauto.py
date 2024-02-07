9d954ec037af1779b570bb98e8c6f0efcd45b4f7
playmaker432 <moonshadow278@gmail.com>
2024-02-07 18:53:58 +0800

    End of Day

--- testauto.py
+++ testauto.py
@@ -12,11 +12,17 @@ stop_automation = threading.Event()
 automation_thread = None  # Global variable to store the thread
 
 
-
 def main():
     global automation_thread
     root = tk.Tk()
     root.withdraw()  # Hide the main window
+
+    pyautogui.moveTo(1350, 1010, duration = 0.3)
+
+    time.sleep(3)
+
+    # Print the screen resolution
+    print(pyautogui.size())
 
     cursor = win32gui.GetCursorInfo()
     cursor_pos = pyautogui.position()
